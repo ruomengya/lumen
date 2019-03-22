49d31f41e8b76a3d50505d6ddf739f34508be8bb
root <root@localhost.localdomain>
2019-03-22 02:26:48 +0000

    ss

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
         $ch = curl_init($url);
 
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch,CURLOPT_HEADER,1);
+        curl_setopt($ch,CURLOPT_POST,1);
         curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
         $res = curl_exec($ch);
         curl_close($ch);
